fa50f7d0bfc59c96501f3d75d957d62c8e4e3219
M-Nandakumar <melamnandakumar70@gmail.com>
2025-02-05 08:24:17 +0530

    Added Day3 File

---
+++ datatypes.js
@@ -0,0 +1,90 @@
+// primitive datatype
+
+// number type
+
+var a = 133
+console.log(a)
+console.log(typeof(a))
+
+var a = 534252356447566
+console.log(a)
+console.log(typeof(a))
+
+var a = 133.987786
+console.log(a)
+console.log(typeof(a))
+
+// ------------------------
+
+// String
+
+var a = 'A'
+console.log(a)
+console.log(typeof(a))
+
+var a = "Nanda"
+console.log(a)
+console.log(typeof(a))
+
+// -------------------------
+
+//boolean
+
+var a = true
+console.log(a)
+console.log(typeof(a))
+
+// // ------------------------------------------------------------------------------------------------------------------------------------------
+
+// null
+var a = null
+console.log(a)
+console.log(typeof(a))
+
+// // undefind
+var a
+console.log(a)
+console.log(typeof(a))
+
+// //----------------------------------------------------------------------------------------
+// // Array
+// // homogeneous
+var a = [1,2,3,4,5]
+console.log(a)
+console.log(typeof(a))
+
+// // heterogeneous
+
+var a = ['A',24364,true]
+console.log(a)
+console.log(typeof(a))
+// //------------------------------------------
+// object 
+
+var a ={
+    name:"nanda",
+    age : 24
+}
+console.log(a)
+console.log(typeof(a))
+
+// //------------------------------------
+
+// array
+
+var ar=[1,2,3,4,5]
+console.log(ar)
+console.log(typeof(ar))
+
+
+var ar=[1,"A",true]
+console.log(ar)
+console.log(typeof(ar))
+// //------------------------------
+// // object 
+var obj={
+    fname:"Anil",
+    age:22
+}
+console.log(obj)
+console.log(typeof(obj))
