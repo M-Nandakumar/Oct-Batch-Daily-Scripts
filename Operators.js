ee01527b8f3271ae860b588cffb82427adbac454
M-Nandakumar <melamnandakumar70@gmail.com>
2025-02-05 08:30:38 +0530

    Added Day4 File

---
+++ Operators.js
@@ -0,0 +1,109 @@
+//Arithmetic operators
+// addition
+var a=10;
+var b=5;
+console.log(a+b)
+
+//substraction
+var a=10;
+var b=5;
+console.log(a-b)
+
+// mul
+var a=10;
+var b=5;
+console.log(a*b)
+
+// exponentiation
+var a=10;
+var b=5;
+console.log(a**b)
+
+// division
+var a=10;
+var b=5;
+console.log(a/b)
+
+//modulus
+var a=10;
+var b=5;
+console.log(a%b)
+
+//increment
+var a=10;
+ a++
+console.log(a)
+
+//decrement
+var a=10;
+a--
+console.log(a)
+
+// -- Assignment Operators ---
+
+var a = 20
+var b = 5
+console.log(a+=b)
+
+var a = 20
+var b = 5
+console.log(a-=b)
+
+var a = 20
+var b = 5
+console.log(a*=b)
+
+var a = 20
+var b = 5
+console.log(a/=b)
+
+var a = 20
+var b = 5
+console.log(a%=b)
+
+var a = 20
+var b = 5
+console.log(a**=b)
+
+//---------------Comparison operator---------
+var a =11
+var b =22
+console.log(a==b)
+
+var a =11
+var b =22
+console.log(a>b)
+
+var a =11
+var b =22
+console.log(a<b)
+
+var a =11
+var b =22
+console.log(a>=b)
+
+var a =11
+var b =22
+console.log(a<=b)
+
+var a =11
+var b =22
+console.log(a!=b)
+
+// String operator
+var str1="hello"
+var str2="guys"
+console.log(str1+" "+str2)
+
+// logical operators
+var a= 123
+var b =321
+console.log((a>b)&&(a<b))
+
+var a =12
+var b =21
+console.log((a>b)||(a<b))
+
+var a =123
+var b =124
+console.log(!(a<b))
