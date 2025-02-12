2cc9e9cc5b6b598772968f67068b98317a014eb1
M-Nandakumar <melamnandakumar70@gmail.com>
2025-02-12 16:26:43 +0530

    Added Day6 File

---
+++ Variables.js
@@ -0,0 +1,29 @@
+// var keyword
+
+{
+    var a = 10
+    console.log(a)
+    a = 20
+    console.log(a)
+}
+a=11+a
+console.log(a)
+// -------------------------------------------------------------------
+//let keyword
+
+{
+    let a = 10
+    console.log(a)
+    a = 20
+    console.log(a)
+}
+a=11+a
+console.log(a)
+
+// ------------------------------------------
+{
+    const a = 10
+    console.log(a)
+}
+  a = 20
+ console.log(a)
