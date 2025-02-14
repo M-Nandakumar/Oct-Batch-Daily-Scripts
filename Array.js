7ea349f35f465dbf8a9aa96e20c3b00cea8577c8
MELAM NANDAKUMAR <melamnandakumar70@gmail.com>
2025-02-14 11:58:21 +0530

    Add files via upload
    Signed-off-by: MELAM NANDAKUMAR <melamnandakumar70@gmail.com>

---
+++ Array.js
@@ -0,0 +1,18 @@
+var arr1 = [10,50.45,true,'Kod',{name:'nanda',age:24},()=>{console.log('function')}]
+console.log('The length of arr1 array:',arr1.length)
+
+
+for(var i=0;i<=arr1.length-1;i++){
+    console.log(`Element at index ${i}`,arr1[i])
+}
+
+//console.log(arr1,typeof arr1)
+
+// Array can store Homogeneous (Data with same data type) as well as Hetrogeneous (Data with diff data type)
+
+console.log('Element at oth index:',arr1[0])
+console.log('Element at 1th index:',arr1[1])
+console.log('Element at 2th index:',arr1[2])
+console.log('Element at 3th index:',arr1[3])
+
+
